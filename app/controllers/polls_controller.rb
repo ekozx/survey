591b9040392d9145af4c3e8510ad4edbf0f8dce0
Evan Kozliner <evankozliner@gmail.com>
2015-03-13 13:19:50 -0400

    kind of a weird in-between commit. Some style changes

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -7,6 +7,12 @@ class PollsController < ApplicationController
     @odh_questions = Question.where(odh: true)
   end
   def edit
+    #Generating a new question
+    @question = Question.new
+    @question.odh = false
+    @question.poll_id = params[:id]
+
+    #For editing old questions
     @poll = Poll.find(params[:id])
     @questions = @poll.questions
     @odh_questions = Question.where(odh: true)
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,9 @@ class QuestionsController < ApplicationController
     Question.find(params[:id]).destroy
     render nothing: true
   end
+  def create
+    Question.create(question: params[:question][:question],
+                    odh: (params[:question][:odh] == 'true'), poll_id: params[:question][:poll].to_i)
+    render nothing: true
+  end
 end
