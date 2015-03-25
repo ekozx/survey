952cb23b164be80f6975d880e7d1a86965fdc3e8
Evan Kozliner <evankozliner@gmail.com>
2015-03-25 13:42:36 -0400

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313223004) do
+ActiveRecord::Schema.define(version: 20150325174155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150313223004) do
     t.datetime "end"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "odh"
   end
 
   add_index "poll_intervals", ["poll_id"], name: "index_poll_intervals_on_poll_id", using: :btree
