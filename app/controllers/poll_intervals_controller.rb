6754ed19d45ad06c649a515ac722f78cf516251a
Evan Kozliner <evankozliner@gmail.com>
2015-03-30 12:09:57 -0400

    in process of question creation --realized I messed up the earlier question migration. Creating a new one

--- app/controllers/poll_intervals_controller.rb
+++ app/controllers/poll_intervals_controller.rb
@@ -1,8 +1,14 @@
 class PollIntervalsController < ApplicationController
   def create
     interval = PollInterval.new(interval_params)
-    interval.start = DateTime.new(params[:start][:year].to_i, params[:start][:month].to_i, params[:start][:day].to_i).to_s
-    interval.end = DateTime.new(params[:end][:year].to_i, params[:end][:month].to_i, params[:end][:day].to_i).to_s
+    #Create the times for start and end
+    #This code is kind of fishy, is there a better way to do this?
+    interval.start = DateTime.new(params[:start][:year].to_i,
+                                  params[:start][:month].to_i,
+                                  params[:start][:day].to_i).to_s
+    interval.end = DateTime.new(params[:end][:year].to_i,
+                                params[:end][:month].to_i,
+                                params[:end][:day].to_i).to_s
     interval.expired = false
 
     if interval.save
@@ -12,8 +18,9 @@ class PollIntervalsController < ApplicationController
     end
   end
   def show
+    @interval = PollInterval.find(params[:id])
   end
-  private
+  protected
   def interval_params
     params.require(:poll_interval).permit(:poll_id, :odh, :expired, :start, :end)
   end
